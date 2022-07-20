9de45fe9c584afc3597c3402f1811ae2b36db486
Ángel Damián Raúl García Guevara <angel.garcia9106@alumnos.udg.mx>
2022-07-19 23:26:31 -0500

    Deleting unneccesary files

---
+++ headers/class.hpp
@@ -0,0 +1,10 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+#include <SFML\Graphics.hpp>
+#include<iostream>
+
+class Class {
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <SFML/Graphics.hpp>
-#include "..\headers\game.hpp"
+#include "..\headers\class.hpp"
 
 int main()
 {
-    Game myGame;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    myGame.run();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
